6fc9f9408e8f0cdb5f63e7efe433abee26146906
jorge varon <tecnol.jorgevaron@gmail.com>
2026-02-19 23:47:02 -0500

    AGREGADO metodo encender coche

--- src/Usuarios.java
+++ src/Usuarios.java
@@ -16,4 +16,20 @@ public class Usuarios {
     static void main() {
 
     }
+    public class Coche {
+        // Atributo para saber el estado del coche
+        private boolean encendido = false;
+
+        /**
+         * Método para encender el coche
+         */
+        public void encenderCoche() {
+            if (!encendido) {
+                encendido = true;
+                System.out.println("El coche se ha encendido. ¡Rum rum!");
+            } else {
+                System.out.println("El coche ya estaba encendido.");
+            }
+        }
+    }
 }
